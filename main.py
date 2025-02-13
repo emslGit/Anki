b637ff44b263cb1b1bfefbf09f2e3090f839a400
Emanuel <45330172+emslGit@users.noreply.github.com>
2025-02-13 04:14:53 +0100

    added so much stuff you cant even imagine

--- main.py
+++ main.py
@@ -12,9 +12,10 @@ import os.path
 # If modifying these scopes, delete the file token.json.
 SCOPES = ["https://www.googleapis.com/auth/spreadsheets.readonly"]
 SAMPLE_RANGE_NAME = "input!A2:F"
-SPREADSHEET_ID="1wEjJpBgwV5LJdWpDxzeC_3u2rUVt3QQQGZXbJNMJSNs"
+SPREADSHEET_ID=""
 MAX_COUNT = 99999
 ACCEPTED_WORD_CLASSES = ['Adjective', 'Adverb', 'Pronoun', 'Preposition', 'Conjunction/subjunction', 'Interjection', 'Phrase', 'Noun', 'Verb']
+ACCEPTED_FREQUENCIES = ['Everyday', 'Common', 'Rare', 'Archaic', 'Regional', 'Incorrect']
 
 client = OpenAI()
 
@@ -72,14 +73,41 @@ def generate_examples(word_es, word, word_class):
         messages=[
             {'role': 'system', 'content': 'You complete missing CSV fields.'},
             {'role': 'user', 'content': f'Use the {word_class} \'{word_es}\' ({word}) in a simple Spanish (castellano) sentence.'},
-            {'role': 'user', 'content': f'Translate {word_es} to both <english> and <swedish>.' + (
+            {'role': 'user', 'content': f'Then translate \'{word_es}\' to both <english> and <swedish>.' + (
                 ' Use definite article adding \'the\' if missing.' if word_class == 'Noun' else
                 ' Use infinitive form adding \'att\' and \'to\' if missing.' if word_class == 'Verb' else 
-                f' Also classify {word_es} as one of: \'Adjective\', \'Adverb\', \'Pronoun\', \'Preposition\', \'Conjunction/subjunction\', \'Interjection\'. If none fits leave it as \'Phrase\'. <word_class> must be exactly one word (or two in the case of \'Conjunction/subjunction\').' if word_class == 'Phrase' else
-                ''
+                f'''
+                Also classify the <word_class> of \'{word_es}\' as one of:
+                    - \'Adjective\',
+                    - \'Adverb\',
+                    - \'Pronoun\',
+                    - \'Preposition\',
+                    - \'Conjunction/subjunction\',
+                    - \'Interjection\'.
+                
+                If none fits **leave it as \'Phrase\'**. <word_class> must be **exactly one word** (or two in the case of \'Conjunction/subjunction\').' if word_class == 'Phrase' else
+                '''
             )},
+            {
+                'role': 'user',
+                'content': f'''
+                    Additionally determine the usage frequency of a phrase or word as one of:
+
+                    - **Everyday** → Constantly used in everyday conversation and informal writing (e.g., "Hola", "O sea", "Sí"). Only give this category to extremely common words.
+                    - **Common** → Frequently used but **less casual** or universal than "Everyday" (e.g., "Desarrollo", "Establecer").
+                    - **Rare** → Slightly more advanced or technical etc. but still used (e.g., "Vituperar", "Desmonte").
+                    - **Archaic** → Outdated of used very rarely, would sound out of place in conversation and writing. Mostly found in older literature or poetry (e.g., "Fuese", "Doquier", "Santo varón").
+                    - **Regional** → Recognized, but mainly used in specific countries (e.g., "Chévere" in Venezuela, "Chamba" in Mexico).
+                    - **Incorrect** → If \'{word_es}\' **is not a real Spanish word or phrase**. 
+
+                    🚨 **IMPORTANT:**  
+                    - Only give a word "Incorrect" in cases with clear grammar and spelling errors, or in cases where it has never been used. Otherwise **classify it as "Archaic"** instead of "Incorrect". Never classify a construct as "Incorrect" because it is infrequent or archaic. If the construct has never been used it is ok to classify it as "Incorrect".
+                    - If unsure of the category, choose "Rare".
+                    - Reply format must be **exactly one word**.
+                '''
+            },
             {'role': 'user', 'content': f'Provide a simple <sentence_in_spanish>, the <sentence_in_english> using \'{word_es}\', and a very brief english <comment> on usage of \'{word_es}\'. Avoid semicolons. Using only the \' symbol for quotes if they are necessary.'},
-            {'role': 'user', 'content': 'It is highly important to stick to the format:<english>|<swedish>|<word_class>|<sentence_in_spanish>|<sentence_in_english>|<comment>'}
+            {'role': 'user', 'content': 'It is highly important to **stick to the following format**:<english>|<swedish>|<word_class>|<sentence_in_spanish>|<sentence_in_english>|<frequency>|<comment>'}
         ]
     )
 
@@ -91,14 +119,20 @@ def generate_examples(word_es, word, word_class):
     _word_class = ''
     example_es = ''
     example_en = ''
+    frequency = ''
     comment = ''
 
     # Split the content by lines and extract the relevant parts
-    word_en, word_sv, _word_class, example_es, example_en, comment = content.split('|')
+    content_split = content.split('|')
+    if len(content_split) != 7:
+        print('Error:', content_split)
+
+    word_en, word_sv, _word_class, example_es, example_en, frequency, comment = content_split
 
     word_class = word_class if is_noun_or_verb else _word_class
     word_class = re.sub(r'["\']+', '', word_class).strip().capitalize()
     word_class = word_class if word_class in ACCEPTED_WORD_CLASSES else 'Phrase'
+    frequency = frequency.strip().capitalize() if frequency.strip().capitalize() in ACCEPTED_FREQUENCIES else 'Incorrect'
 
     return {
         'Spanish': capitalize_first_alphanumeric(word_es),
@@ -107,8 +141,65 @@ def generate_examples(word_es, word, word_class):
         'Class': word_class,
         'Example ES': re.sub(r'["\']+', '', example_es).strip(),
         'Example EN': re.sub(r'["\']+', '', example_en).strip(),
+        'Frequency': frequency,
         'Comment': re.sub(r'["\']+', '', comment).strip(),
     }
+
+def generate_frequencies(output_file):
+    df_out = pd.read_csv(output_file, delimiter=';')
+    count_added = 0
+    batch_size = 20
+    batch_number = 0
+    offset = 0
+
+    for index, row in df_out.iterrows():
+        if offset <= index:         
+            word_es = row['Spanish']
+            word_en = row['English']
+            word_se = row['Swedish']
+            word_class = row['Class']
+
+            response = client.chat.completions.create(
+                model='gpt-4o-mini',
+                messages=[
+                    {
+                        'role': 'system',
+                        'content': f'''
+                            You determine the usage frequency of a phrase or word as one of:
+
+                            - **Everyday** → Constantly used in everyday conversation and informal writing (e.g., "Hola", "O sea", "Sí"). Only give this category to extremely common words.
+                            - **Common** → Frequently used but **less casual** or universal than "Everyday" (e.g., "Desarrollo", "Establecer").
+                            - **Rare** → Slightly more advanced or technical etc. but still used (e.g., "Vituperar", "Desmonte").
+                            - **Archaic** → Outdated of used very rarely, would sound out of place in conversation and writing. Mostly found in older literature or poetry (e.g., "Fuese", "Doquier", "Santo varón").
+                            - **Regional** → Recognized, but mainly used in specific countries (e.g., "Chévere" in Venezuela, "Chamba" in Mexico).
+                            - **Incorrect** → If \'{word_es}\' **is not a real Spanish word or phrase**. 
+
+                            🚨 **IMPORTANT:**  
+                            - Only give a word "Incorrect" in cases with clear grammar and spelling errors, or in cases where it has never been used. Otherwise **classify it as "Archaic"** instead of "Incorrect". Never classify a construct as "Incorrect" because it is infrequent or archaic. If the construct has never been used it is ok to classify it as "Incorrect".
+                            - If unsure of the category, choose "Rare".
+                            - Reply format must be **exactly one word**.
+                        '''
+                    },
+                    {
+                        'role': 'user',
+                        'content': f'What is the usage frequency for the \'{word_class}\' \'{word_es}\' meaning \'{word_en}\' in English and \'{word_se}\' in Swedish?'
+                    }
+                ]
+            )
+
+            frequency_parsed = response.choices[0].message.content.strip().capitalize()
+            frequency = 'Incorrect' if frequency_parsed not in ACCEPTED_FREQUENCIES else frequency_parsed
+            df_out.at[index, 'Tags'] = ' '.join(
+                filter(None, [str(row['Tags']) if pd.notna(row['Tags']) else '', str(frequency).strip()])
+            ).strip()
+            count_added += 1
+            print(f"{index}, Frequency for {row['Spanish']} is {frequency}")
+        if (count_added + 1) % batch_size == 0:
+            df_out.to_csv('output.csv', index=False, sep=';', quotechar='"', quoting=csv.QUOTE_ALL)
+            print(f'{index}: saving batch {batch_number}, modified {count_added}.')
+
+    df_out.to_csv('output.csv', index=False, sep=';', quotechar='"', quoting=csv.QUOTE_ALL)
+    print(f'Completed {count_added} rows out of a total of {len(df_out)}.')
 
 def generate_word_classes(output_file):
     df_out = pd.read_csv(output_file, delimiter=';')
@@ -155,18 +246,18 @@ def process_csv(output_file):
     else:
         df_in_lower = df_in.copy()
         df_out_lower = df_out.copy()
-        print(df_in_lower)
         df_in_lower['Spanish'] = df_in['Spanish'].str.lower()
         df_out_lower['Spanish'] = df_out['Spanish'].str.lower()
         diff_rows = df_in[~df_in_lower['Spanish'].isin(df_out_lower['Spanish'])].copy()
 
         # Replace "x" with the column name, keep empty otherwise
+        df_in['Spanish'] = df_in['Spanish'].apply(capitalize_first_alphanumeric)
         df_in['Eman'] = df_in['Eman'].apply(lambda x: 'Eman' if x == 'x' else '')
         df_in['Sixten'] = df_in['Sixten'].apply(lambda x: 'Sixten' if x == 'x' else '')
 
         # Merge new rows and drop "Eman" and "Sixten" columns
         df_out = pd.concat([df_out, diff_rows], ignore_index=True).drop_duplicates(subset=['Spanish'], keep='first').drop(columns=['Eman', 'Sixten'], errors='ignore')
-
+        
         # Map Tags correctly from df_in
         df_out['Tags_Tmp'] = ''
         df_out['Tags_Tmp'] = df_out['Spanish'].map(
@@ -175,6 +266,13 @@ def process_csv(output_file):
         ).fillna(df_out['Tags_Tmp'])
 
     for index, row in df_out.iterrows():
+        tags = str(df_out.at[index, 'Tags']).split(" ") if not pd.isna(df_out.at[index, 'Tags']) else []
+        tags_set = set(tags)
+        tags_set.discard('Eman')
+        tags_set.discard('Sixten')
+        tags = list(tags_set)
+        tags_tmp = df_out.at[index, 'Tags_Tmp'].split(" ")
+
         if index < offset:
             continue
 
@@ -184,9 +282,10 @@ def process_csv(output_file):
             df_out.at[index, 'Error'] = 'Spanish required'
             continue
 
-        word_es = capitalize_first_alphanumeric(row['Spanish'])            
+        word_es = row['Spanish']
         word_class = row['Class'] if not pd.isna(row['Class']) else 'Phrase'
-    
+        
+        frequency = ''
         word_en = None
         word_sv = None
         
@@ -209,6 +308,7 @@ def process_csv(output_file):
         if is_new:
             examples = generate_examples(word_es, word_en if word_en else word_sv, word_class)
             word_class = examples['Class']
+            frequency = examples['Frequency']
 
             print(f"{examples['Spanish']} - {examples['Class']} - {examples['Example ES']} - {examples['Example EN']}")
             
@@ -223,10 +323,11 @@ def process_csv(output_file):
             df_out.at[index, 'Example EN'] = examples['Example EN']
             df_out.at[index, 'Comment'] = examples['Comment']
             count_added += 1
-            
-        df_out.at[index, 'Tags'] = ' '.join(
-            filter(None, [str(row['Tags_Tmp']) if pd.notna(row['Tags_Tmp']) else '', str(word_class).strip()])
-        ).strip()
+        
+        unique_tags = list(set(tags + tags_tmp + [word_class, frequency]))
+        unique_tags.remove('')
+        unique_tags.sort()
+        df_out.at[index, 'Tags'] = ' '.join(filter(None, unique_tags)).strip()
 
         if (count_added + 1) % batch_size == 0:
             df_out.to_csv(output_file, index=False, sep=';', quotechar='"', quoting=csv.QUOTE_ALL)
@@ -264,3 +365,4 @@ if __name__ == '__main__':
     # default = normalize_csv('Default.txt', 'default_normalized.csv', read_delimiter='\t', write_delimiter=';', skip_rows=2)
     # compare_dfs(output, default)
     # generate_word_classes('output.csv')
+    # generate_frequencies('output.csv')
